a06651089e3f32b8816bde452e656538ceeafa58
AJ Behncke <ajbinky@gmail.com>
2017-03-21 12:16:21 -0500

    number 9

--- chapter9.sql
+++ chapter9.sql
@@ -12,3 +12,10 @@
 -- Problem 3
 
 --problem 4
+
+-- Problem 9
+select b.title, oi.order#, c.state
+from books b left join orderitems oi on b.isbn=oi.isbn
+	left join orders o on oi.order#=o.order#
+	left join customers c = o.customer#=c.customer#
+order by 1; --first column
